28bc0cb615c326339fef2efb574aaad426bb2868
nesfez21 <nesfez21@htl-kaindorf.at>
2026-02-19 03:02:56 +0100

    Vorheizregelung

---
+++ controller/controller_preheat.py
@@ -0,0 +1,40 @@
+from gpiozero import OutputDevice
+from time import sleep
+
+class PreheatController():
+    def __init__(self, heater_pin, setpoint):
+        self.setpoint = setpoint
+        self.Kp = 5.0
+        self.Ki = 0.1
+        self.windup_limit = 100.0
+        self.window_time = 5.0
+
+        self.heater = OutputDevice(heater_pin)
+        self.integral = 0.0
+
+    def calculate_control_signal(self, current_temp):
+        error = self.setpoint - current_temp
+        dt = self.window_time
+        self.integral += error * dt
+        self.integral = max(-self.windup_limit,
+                        min(self.windup_limit, self.integral))
+
+        control_signal = self.Kp * error + self.Ki * self.integral
+        # Begrenzen auf 0–100 %
+        control_signal = max(0, min(100, control_signal))
+        print(f"Error: {error:.2f}, Integral: {self.integral:.2f}")
+
+        return control_signal
+
+    def control_heater(self, control_signal):
+        on_time = (control_signal / 100.0) * self.window_time
+        off_time = self.window_time - on_time
+
+        if on_time > 0:
+            self.heater.on()
+            sleep(on_time)
+
+        if off_time > 0:
+            self.heater.off()
+            sleep(off_time)
+        
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import time
 from temp.temperature import temperature
+from controller.controller_preheat import PreheatController
 
 #Sensor-IDs
 SENSOR_1 = "28-00000050b91c"
 SENSOR_2 = "28-00000052834d"
 
 HEATER_PIN = 5
-SETPOINT = 30.0
+SETPOINT = 38.0
 
 def main():
-    measure = temperature(SENSOR_1, SENSOR_2)
-    temp1 = measure[0]
-    temp2 = measure[1]
-    print(f"Temperatur Sensor 1: {temp1:.2f} °C")
-    print(f"Temperatur Sensor 2: {temp2:.2f} °C")
+    preheat_controller = PreheatController(HEATER_PIN, SETPOINT)
+
+    while True:
+        measure = temperature(SENSOR_1, SENSOR_2)
+        temp1 = measure[0]
+        temp2 = measure[1]
+        print(f"Temperatur Sensor 1: {temp1:.2f} °C")
+        print(f"Temperatur Sensor 2: {temp2:.2f} °C")
+
+        control_signal = preheat_controller.calculate_control_signal(temp2)
+        print(f"Heizleistung: {control_signal:.2f} %")
+        preheat_controller.control_heater(control_signal)
 
 if __name__ == "__main__":
     main()
